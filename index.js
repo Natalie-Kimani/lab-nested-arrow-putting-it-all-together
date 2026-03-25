860c74ebc30300c10b236987c7a1835d29e18bae
Natalie Kimani <anitakimani07@gmail.com>
2026-03-25 10:08:04 +0300

    Arrows and Nested if

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+function createLoginTracker (userInfo){  
+    let username = "user 1";
+    let password = "password!";
+    let attemptCount = 0;
+    let maxatttemptCount = 3;
+    return (passwordAttempt) => {
+      attemptCount++
 
+      if (attemptCount >3) {
+         console.log("Account is locked!");
+       return "Account locked due to too many failed login attempts"
+      }
 
+      if (passwordAttempt === userInfo.password ){
+        console.log("Password correct!");
+      return "Login successful";
+      }
+         console.log("Password incorrect.");
+        return `Attempt ${attemptCount}: Login failed`;
+    }
+  }
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
